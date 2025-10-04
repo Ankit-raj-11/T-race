adaf8ccb9acef81a28316735c13b6c977896c522
Sachinxmpl <sachinxmpl6@gmail.com>
2025-10-04 21:15:13 +0545

    feat:add real time typing feedback

--- src/pages/race.js
+++ src/pages/race.js
@@ -35,6 +35,57 @@ export default function Race() {
     fetchNewSentence();
   };
 
+  const getCharacterStatus = (index) => {
+    if (index >= userInput.length){
+      return 'untyped';
+    }
+    if (userInput[index] === sentence[index]){
+      return 'correct';
+    }
+    return 'incorrect';
+  }
+
+  const renderSentence = () => {
+    const chars = sentence.split('');
+    const extraChars = userInput.length > sentence.length? userInput.slice(sentence.length) : '';
+
+    return (
+      <div className="text-lg leading-relaxed font-mono flex flex-wrap">
+        {chars.map((char, index) => {
+          const status = getCharacterStatus(index);
+          let colorClass = 'text-white';
+          
+          if (status === 'correct') {
+            colorClass = 'text-emerald-400';
+          } else if (status === 'incorrect') {
+            colorClass = 'text-red-300 bg-red-500/20 rounded px-0.5';
+          }
+
+          const isCurrentChar = index === userInput.length;
+          const cursorClass = isCurrentChar ? 'border-l-2 border-cyan-400 animate-pulse' : '';
+
+          return (
+            <span
+              key={index}
+              className={`${colorClass} ${cursorClass} ${char === ' ' ? 'mx-0.5' : ''}`}
+            >
+              {char === ' ' ? '\u00A0' : char}
+            </span>
+          );
+        })}
+        {extraChars && (
+          <span className="text-red-400 bg-red-400/30 rounded px-1 border-b-2 border-red-500">
+            {extraChars.split('').map((char, i) => (
+              <span key={`extra-${i}`}>
+                {char === ' ' ? '\u00A0' : char}
+              </span>
+            ))}
+          </span>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div className="bg-gray-900 text-white">
       {/* Background Pattern */}
@@ -77,9 +128,7 @@ export default function Race() {
                     <span className="ml-3 text-gray-400">Loading sentence...</span>
                   </div>
                 ) : (
-                  <p className="text-lg leading-relaxed text-gray-200 font-mono">
-                    {sentence}
-                  </p>
+                  renderSentence()
                 )}
               </div>
             </div>
