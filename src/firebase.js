2c0fbeee3b4308b5acb663f06374ebc56f9f6755
Ankit raj <ankit74850raj@gmail.com>
2025-10-05 10:31:20 +0530

    .env and .en.example set

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey: "AIzaSyDXTE8avOZYsz-mAZDqVdrO2YS0EwyTAZU",
-  authDomain: "t-race-b619c.firebaseapp.com",
-  projectId: "t-race-b619c",
-  storageBucket: "t-race-b619c.firebasestorage.app",
-  messagingSenderId: "234365029319",
-  appId: "1:234365029319:web:928320cf1612be8feee7e2",
-  measurementId: "G-CRLSK8H0EY",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
