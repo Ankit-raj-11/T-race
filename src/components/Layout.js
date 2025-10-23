22193fdd12c82ac1026061af010a115601a488c2
Ankit raj <ankit74850raj@gmail.com>
2025-10-23 16:12:18 +0530

    SpeedInsight added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,7 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
 SpeedInsights();
-
-
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-900">
